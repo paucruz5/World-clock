73f7591548532ffe4bc2bf351ae761df8dedee9c
Pau Cruz <paulinamc_micki@hotmail.com>
2024-02-23 22:51:28 -0600

    selection of cities
    every time you select a new city, date, time and name is going to replace the former cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,36 +1,65 @@
 function updateTime() {
   let cuiaba = document.querySelector("#cuiaba");
-  let cuiabaDate = cuiaba.querySelector(".date");
-  let cuiabaTime = cuiaba.querySelector(".time");
-  let cuiabaDT = moment().tz("America/Cuiaba");
+  if (cuiaba) {
+    let cuiabaDate = cuiaba.querySelector(".date");
+    let cuiabaTime = cuiaba.querySelector(".time");
+    let cuiabaDT = moment().tz("America/Cuiaba");
 
-  cuiabaDate.innerHTML = cuiabaDT.format("MMMM Do YYYY");
-  cuiabaTime.innerHTML = cuiabaDT.format("h:mm:ss [<small>]A[</small>]");
+    cuiabaDate.innerHTML = cuiabaDT.format("MMMM Do YYYY");
+    cuiabaTime.innerHTML = cuiabaDT.format("h:mm:ss [<small>]A[</small>]");
+  }
 
-  let tizimin = document.querySelector("#tizimin");
-  let tiziminDate = tizimin.querySelector(".date");
-  let tiziminTime = tizimin.querySelector(".time");
-  let tiziminDT = moment().tz("America/Merida");
+  let merida = document.querySelector("#merida");
+  if (merida) {
+    let meridaDate = merida.querySelector(".date");
+    let meridaTime = merida.querySelector(".time");
+    let meridaDT = moment().tz("America/Merida");
 
-  tiziminDate.innerHTML = tiziminDT.format("MMMM Do YYYY");
-  tiziminTime.innerHTML = tiziminDT.format("h:mm:ss [<small>]A[</small>]");
+    meridaDate.innerHTML = meridaDT.format("MMMM Do YYYY");
+    meridaTime.innerHTML = meridaDT.format("h:mm:ss [<small>]A[</small>]");
+  }
 
   let lagos = document.querySelector("#lagos");
-  let lagosDate = lagos.querySelector(".date");
-  let lagosTime = lagos.querySelector(".time");
-  let lagosDT = moment().tz("Africa/Lagos");
+  if (lagos) {
+    let lagosDate = lagos.querySelector(".date");
+    let lagosTime = lagos.querySelector(".time");
+    let lagosDT = moment().tz("Africa/Lagos");
 
-  lagosDate.innerHTML = lagosDT.format("MMMM Do YYYY");
-  lagosTime.innerHTML = lagosDT.format("h:mm:ss [<small>]A[</small>]");
+    lagosDate.innerHTML = lagosDT.format("MMMM Do YYYY");
+    lagosTime.innerHTML = lagosDT.format("h:mm:ss [<small>]A[</small>]");
+  }
 
-  let lesbos = document.querySelector("#lesbos");
-  let lesbosDate = lesbos.querySelector(".date");
-  let lesbosTime = lesbos.querySelector(".time");
-  let lesbosDT = moment().tz("Europe/Athens");
+  let athens = document.querySelector("#athens");
+  if (athens) {
+    let athensDate = athens.querySelector(".date");
+    let athensTime = athens.querySelector(".time");
+    let athensDT = moment().tz("Europe/Athens");
 
-  lesbosDate.innerHTML = lesbosDT.format("MMMM Do YYYY");
-  lesbosTime.innerHTML = lesbosDT.format("h:mm:ss [<small>]A[</small>]");
+    athensDate.innerHTML = athensDT.format("MMMM Do YYYY");
+    athensTime.innerHTML = athensDT.format("h:mm:ss [<small>]A[</small>]");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+       <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+      </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelection = document.querySelector("#city");
+
+citiesSelection.addEventListener("change", updateCity);
